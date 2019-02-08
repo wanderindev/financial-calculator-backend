7a4b61a353f9d3726c423b555e51e3bf12f1cc0f
Javier Feliu <jfeliu@aesa.biz>
2019-02-07 20:50:45 -0500

    feat:  Add function to calculate real interest rate

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import ceil, log
 
 
 def get_balances(periods, deposits, ini_dep, rate, freq,
@@ -108,3 +108,61 @@ def parse_args(kwargs):
     kwargs['time_scale'] = get_time_scale(kwargs['freq'])
 
     return kwargs
+
+
+def fut_val(r, nper, pv):
+    return -pv * pow((1 + r), nper)
+
+
+def nper(r, payment, pv):
+    return log(pow((1 - (pv * r) / payment), -1)) / log(1 + r)
+
+
+def payment(r, nper, pv, fv=0, t=0):
+    q = pow(1 + r, nper)
+
+    return - (r * (fv + (q * pv))) / ((-1 + q) * (1 + r * t))
+
+
+def pres_val(r, nper, fv):
+    return -fv / pow((1 + r), nper)
+
+
+def pres_val_annuity(r, nper, payment):
+    return payment * (1 - pow((1 + r), -1 * nper)) / r
+
+
+def interest_rate(nper, pmt, pv, fv=0, t=0, guess=0.1):
+    low = 0
+    high = 1
+    tolerance = abs(0.00000005 * pmt) or 0.001
+
+    # Try to find a solution in 60 iterations.
+    for i in range(60):
+        # Reset the balance to the original pv.
+        balance = pv
+
+        # Calculate the final balance, based on loan conditions.
+        for j in range(nper):
+            if not t:
+                # Apply interests before payment
+                balance = balance * (1 + guess) + pmt
+            else:
+                # Apply payment before interests
+                balance = (balance + pmt) * (1 + guess)
+
+        # Return success and guess if balance is within tolerance.
+        if abs(balance + fv) < tolerance:
+            return True, guess
+        elif balance + fv > 0:
+            # Adjust high, since guess was too big.
+            high = guess
+        else:
+            # Adjust low, since guess was too small.
+            low = guess
+
+        # Calculate a new guess.
+        guess = (high + low) / 2
+
+    # Return success False, rate None, since no acceptable result was found.
+    return False, None
