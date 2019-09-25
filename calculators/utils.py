f9bcf62f56a50216e3dd39562d3480ee947b0b2d
Javier Feliu <javier@wanderin.dev>
2019-09-24 23:51:13 -0500

    fix: Fix bug with numbering of periods

--- calculators/utils.py
+++ calculators/utils.py
@@ -6,7 +6,7 @@ def format_tables(calculator, ff, _type):
     ff = int(ff)
 
     if ff > 0 and _type is 'cc':
-        return [dict(p='{:0,.0f}'.format(calculator.periods[x-1]),
+        return [dict(p='{:0,.0f}'.format(x / ff),
                      pt='{:0,.2f}'.format(
                          sum(calculator.payments[x - ff:x])),
                      pp='{:0,.2f}'.format(
@@ -16,14 +16,14 @@ def format_tables(calculator, ff, _type):
                 for x in calculator.periods if x % ff == 0]
 
     if ff > 0 and _type is 'savings':
-        return [dict(p='{:0,.0f}'.format(calculator.periods[x-1]),
+        return [dict(p='{:0,.0f}'.format(x / ff),
                      d='{:0,.2f}'.format(sum(calculator.deposits[x-ff:x])),
                      i='{:0,.2f}'.format(sum(calculator.interests[x-ff:x])),
                      b='{:0,.2f}'.format(calculator.balances[x-1]))
                 for x in calculator.periods if x % ff == 0]
 
     if ff > 0 and _type is 'loans':
-        return [dict(p='{:0,.0f}'.format(calculator.periods[x-1]),
+        return [dict(p='{:0,.0f}'.format(x / ff),
                      pe='{:0,.2f}'.format(sum(calculator.payments_e[x-ff:x])),
                      pr='{:0,.2f}'.format(
                          sum(calculator.payments_r[x - ff:x])),
@@ -36,7 +36,7 @@ def format_tables(calculator, ff, _type):
                 for x in calculator.periods if x % ff == 0]
 
     if ff > 0 and _type is 'retirements':
-        return [dict(p='{:0,.0f}'.format(calculator.periods[x-1]),
+        return [dict(p='{:0,.0f}'.format(x / ff),
                      w='{:0,.2f}'.format(sum(calculator.withdrawals[x-ff:x])),
                      i='{:0,.2f}'.format(sum(calculator.interests[x-ff:x])),
                      b='{:0,.2f}'.format(calculator.balances[x-1]))
